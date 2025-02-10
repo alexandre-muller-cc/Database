c39e7903ef82d62d01a2ab9da637bb5ee54f0731
Alexandre Muller <95938535+alexandre-muller-cc@users.noreply.github.com>
2025-02-10 14:51:57 -0500

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -24,11 +24,41 @@ CREATE TABLE Personnel (
     Province VARCHAR(50),
     Postal_Code VARCHAR(6),
     email VARCHAR(50),
-    Role ENUM('General Manager','Captain','Treasurer','Coach') NOT NULL
+    Role ENUM('General Manager','Captain','Treasurer','Coach') NOT NULL,
     Mandate ENUM('Salaried','Volunteer') NOT NULL,
     Locations VARCHAR(50),
     `Start of operation` DATE NOT NULL ,
     `End of operation` DATE,
-    
-    
+    PRIMARY KEY(`MEDICAR CART NUMBER`)
 );
+
+
+CREATE TABLE Family_Member (
+    `First Name` VARCHAR(50), 
+    `Last Name` VARCHAR(50), 
+    `Date of Birth` DATE,
+    SIN VARCHAR(50) NOT NULL,
+    `MEDICAR CART NUMBER` VARCHAR(50),
+    Phone_Number VARCHAR(12),
+    Address VARCHAR(50),
+    City VARCHAR(50),
+    Province VARCHAR(50),
+    Postal_Code VARCHAR(6),
+    email VARCHAR(50),
+    Locations VARCHAR(50),
+    `Start of operation` DATE NOT NULL ,
+    `End of operation` DATE,
+    Relationship ENUM('Father','Mother','Friend','Partner','Grandfather','Grandmother','Tutor'),
+    PRIMARY KEY(`MEDICAR CART NUMBER`)
+);
+
+CREATE TABLE ClubMember(
+    
+    )
+
+
+
+
+
+
+
